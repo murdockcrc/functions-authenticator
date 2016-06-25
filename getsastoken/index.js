e10cfa496617bc29e50070203df900dc1aa9c57d
Luis Delgado <murdockcrc@Luiss-MacBook-Pro.local>
2016-06-25 09:03:32 +0200

    .

--- getsastoken/index.js
+++ getsastoken/index.js
@@ -1,11 +1,7 @@
 var crypto = require('crypto');
 var util = require('util');
-var request = require('request');
 
 var sbNamespace = process.env.SBNAMESPACE || 'cbpi-prod.azure-devices.net';
-var sbEntityPath = process.env.SBENTITYPATH || 'cbpi-prod';
-var sharedAccessKey = process.env.SENDERS_SHARED_ACCESS_KEY_1 || 'KsUrYkkTxq8GOI45/RntXenqo2+6VreIG/WBf3PBRJo='; 
-var sharedAccessKeyName = process.env.SENDER_SHARED_ACCESS_NAME_1 || 'SendersPolicy';
 
 var generateSasToken = function(resourceUri, signingKey, policyName, expiresInMins) {
     resourceUri = encodeURIComponent(resourceUri.toLowerCase()).toLowerCase();
@@ -30,13 +26,13 @@ var generateSasToken = function(resourceUri, signingKey, policyName, expiresInMi
     return token;
 };
 
-module.exports.getSASToken = function getSASToken(req, res) { 
+module.exports.getSASToken = function getSASToken(context, req) { 
     var token = req.query.token;
     var deviceId = req.query.deviceId;
     var resourceUri = util.format('%s/devices/%s', sbNamespace, deviceId);
 
     var sasToken = generateSasToken(resourceUri, token, null, 14400);
 
-    console.log(sasToken);
-    res.send(sasToken);    
+    context.log(sasToken);
+    context.send(sasToken);    
 }
