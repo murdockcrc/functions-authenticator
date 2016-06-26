b620bd21c230eb14dc660da1afee51fd35f3d568
Luis Delgado <murdockcrc@Luiss-MacBook-Pro.local>
2016-06-26 22:22:58 +0200

    improved unit testing

--- registerNewDevice/test.js
+++ registerNewDevice/test.js
@@ -1,29 +1,52 @@
 var func = require('./index.js'),
-    should = require('should');
+    should = require('should'),
+    uuid = require('uuid');
+
+function getResponseObject() {
+    return {
+        res: null,
+        log: function(message) {
+            console.log(message);
+        },
+        done: null
+    }
+}
 
 describe('Register new device', function(){
   it('Register new device', function(done){
-    this.timeout(3000);
+    this.timeout(5000);
 
-    var deviceId = "cbpi-xxxx";
+    var uniqueId = uuid.v1();
+    var deviceId = "test-" + uniqueId;
     var req = {
         query: {
             deviceId: deviceId
         }
     }
+    var context = getResponseObject();
+    context.done = function() {
+        context.res.status.should.equal(201);
+        done();
+    };
 
-    func(
-        {
-            res: function(params) {
-                console.log(params);
-                params.status.should.equal(201);
-                done();
-            },
-            log: function(message) {
-                console.log(message);
-            },
-            done: function() { }
+    func(context, req);
+  });
+
+  it('Register conflicted device', function(done){
+    this.timeout(5000);
+
+    var deviceId = "cbpi-xxx";
+    var req = {
+        query: {
+            deviceId: deviceId
         }
-    , req);
+    }
+    var context = getResponseObject();
+    context.done = function() {
+        this.res.status.should.equal(500);
+        done();
+    }
+
+    func(context, req);
   });
 });
