17ad351cd4ce5ead3374908c0099d3c5d54e7aa5
Luis Delgado <murdockcrc@Luiss-MacBook-Pro.local>
2016-06-27 14:31:58 +0200

    added unit testing to getSASToken function improved getSASToken input validation

--- getSASToken/test.js
+++ getSASToken/test.js
@@ -1,6 +1,5 @@
 var func = require('./index.js'),
-    should = require('should'),
-    uuid = require('uuid');
+    should = require('should');
 
 function getResponseObject() {
     return {
@@ -12,20 +11,80 @@ function getResponseObject() {
     }
 }
 
-describe('Register new device', function(){
-
-  it('Register conflicted device', function(done){
+describe('getSASToken', function(){
+  it('Gets SAS token', function(done){
     this.timeout(5000);
 
-    var deviceId = "cbpi-xxx";
-    var req = {
-        query: {
-            deviceId: deviceId
+    var deviceId = "cbpi-xxx",
+        token = "CFioZg5ajh6+qX1p1tZx4J4PsRKc5WdaCogVjSmSkVI=",
+        req = {
+            query: {
+                deviceId: deviceId,
+                token: token
         }
     }
     var context = getResponseObject();
     context.done = function() {
-        this.res.status.should.equal(500);
+        this.res.status.should.equal(200);
+        this.res.body.should.be.type('string');
+        done();
+    }
+
+    func(context, req);
+  });
+
+  it('Tries to get SAS token without Device ID', function(done){
+    this.timeout(5000);
+
+    var deviceId = "cbpi-xxx",
+        token = "CFioZg5ajh6+qX1p1tZx4J4PsRKc5WdaCogVjSmSkVI=",
+        req = {
+            query: {
+                token: token
+        }
+    }
+    var context = getResponseObject();
+    context.done = function() {
+        this.res.status.should.equal(400);
+        this.res.body.should.equal('MissingDeviceId');
+        done();
+    }
+
+    func(context, req);
+  });
+
+  it('Tries to get SAS token without token', function(done){
+    this.timeout(5000);
+
+    var deviceId = "cbpi-xxx",
+        token = "CFioZg5ajh6+qX1p1tZx4J4PsRKc5WdaCogVjSmSkVI=",
+        req = {
+            query: {
+                deviceId: deviceId,
+        }
+    }
+    var context = getResponseObject();
+    context.done = function() {
+        this.res.status.should.equal(400);
+        this.res.body.should.equal('MissingAuthenticationToken');
+        done();
+    }
+
+    func(context, req);
+  });
+
+  it('Tries to get SAS token without query strings', function(done){
+    this.timeout(5000);
+
+    var deviceId = "cbpi-xxx",
+        token = "CFioZg5ajh6+qX1p1tZx4J4PsRKc5WdaCogVjSmSkVI=",
+        req = {
+    }
+
+    var context = getResponseObject();
+    context.done = function() {
+        this.res.status.should.equal(400);
+        this.res.body.should.equal('NoBodyProvided');
         done();
     }
 
