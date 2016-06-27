234a63ac02a06f4404b5d678d14df24a3a204180
Luis Delgado <luis.delgado@codit.eu>
2016-06-27 06:54:23 +0200

    added authentication input as token

--- registerNewDevice/index.js
+++ registerNewDevice/index.js
@@ -5,7 +5,7 @@ var util = require('util');
 var request = require('request');
 
 var iotHubName = process.env.IOT_HUB_NAME || 'cbpi-prod.azure-devices.net';
-var iotHubConnectionString = process.env.IOTHUB_CONNECTION_STRING || util.format('HostName=cbpi-prod.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=CaIZWxaQdRBT09euPaaQBbbToEWfuv1Jj38QRxE3RYo=');
+var iotHubConnectionString;
 
 function provisionDevice(deviceId, callback) { 
     var registry = iothub.Registry.fromConnectionString(iotHubConnectionString);
@@ -16,21 +16,46 @@ function provisionDevice(deviceId, callback) {
     registry.create(device, callback);
 }
 
-module.exports = function (context, req) {
-    var deviceId = req.query.deviceId;
-
-    provisionDevice(deviceId, function(error, deviceInfo) {
-        if (error) {
-            context.res = {
-                status: 500,
-                body: "An error was received from the backend when trying to register the device"
-            };
-        } else {
-            context.res = {
-                status: 201,
-                body: deviceInfo
-            };
+function validateInput(deviceId, token) {
+    var result = null;
+    if(!deviceId) {
+        result = {
+            status: 400,
+            body: "MissingDeviceId"
+        };
+    } else if(!token) {
+        result = {
+            status: 400,
+            body: "MissingAuthenticationToken"
         }
+    }
+    return result;
+}
+
+module.exports = function (context, req) {
+    var deviceId = req.body.deviceId,
+        token = req.body.token;
+
+    var isInputValid = validateInput(deviceId, token);
+    if(isInputValid) {
+        context.res = isInputValid;
         context.done();
-    });
+    } else {
+        iotHubConnectionString = process.env.IOTHUB_CONNECTION_STRING || util.format('HostName=cbpi-prod.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=%s', token);
+
+        provisionDevice(deviceId, function(error, deviceInfo) {
+            if (error) {
+                context.res = {
+                    status: 500,
+                    body: "An error was received from the backend when trying to register the device"
+                };
+            } else {
+                context.res = {
+                    status: 201,
+                    body: deviceInfo
+                };
+            }
+            context.done();          
+        });
+    }
 }
--- registerNewDevice/test.js
+++ registerNewDevice/test.js
@@ -16,11 +16,13 @@ describe('Register new device', function(){
   it('Register new device', function(done){
     this.timeout(5000);
 
+    var token = "CaIZWxaQdRBT09euPaaQBbbToEWfuv1Jj38QRxE3RYo=";
     var uniqueId = uuid.v1();
     var deviceId = "test-" + uniqueId;
     var req = {
-        query: {
-            deviceId: deviceId
+        body: {
+            deviceId: deviceId,
+            token: token
         }
     }
     var context = getResponseObject();
@@ -36,9 +38,33 @@ describe('Register new device', function(){
     this.timeout(5000);
 
     var deviceId = "cbpi-xxx";
+    var token = "token";
+
     var req = {
-        query: {
-            deviceId: deviceId
+        body: {
+            deviceId: deviceId,
+            token: token
+        }
+    }
+    var context = getResponseObject();
+    context.done = function() {
+        this.res.status.should.equal(500);
+        done();
+    }
+
+    func(context, req);
+  });
+
+  it('Register device with wrong authentication token', function(done){
+    this.timeout(5000);
+
+    var deviceId = "cbpi-xxx";
+    var token = "token";
+
+    var req = {
+        body: {
+            deviceId: deviceId,
+            token: token
         }
     }
     var context = getResponseObject();
