2cb7214eebfb47c0e2438e42cb5e302f82fa62dd
Luis Delgado <murdockcrc@Luiss-MacBook-Pro.local>
2016-06-28 07:57:28 +0200

    added unit test to getJsonWebToken

--- getJsonWebToken/test.js
+++ getJsonWebToken/test.js
@@ -15,7 +15,7 @@ describe('getJsonWebToken', function(){
     var username = "user1@coditlab.onmicrosoft.com",
         password = process.env.AAD_USER_PASSWORD;
 
-    it('AAD Server to Server Authentication', function(done){
+    it('AAD user Authentication', function(done){
         this.timeout(5000);
 
         var req = {
@@ -30,6 +30,24 @@ describe('getJsonWebToken', function(){
             response.should.not.be.empty;
             response.should.have.property('tokenType');
             response.userId.should.equal(username);
+            done();
+        }
+
+        func(context, req);
+    });
+
+    it('AAD user Authentication - wrong password', function(done){
+        this.timeout(5000);
+
+        var req = {
+            body: {
+                username: username,
+                password: "password"
+            }
+        }
+        var context = getResponseObject();
+        context.done = function() {
+            this.res.status.should.equal('400');            
             done();
         }
 
